6152b4f7a516ee91b4b2d2fcf497204ebab5734a
Bartosz Złotnik <bartekzlotnik@gmail.com>
2023-10-21 19:46:12 +0200

    naprawiono wyszukiwanie do miasta

--- backend/api_translation.py
+++ backend/api_translation.py
@@ -29,6 +29,8 @@ class ApiTranslation():
         self.disabled = request.get('disabled', '')
         self.elevator = request.get('elevator', '')
 
+        self.locality = self.html_parse(self.locality)
+
     def html_parse(self, to_parse: str):
 
         return urllib.parse.quote(to_parse)
@@ -61,11 +63,12 @@ class ApiTranslation():
 
     def generating_api_command(self):
         
-        self.api_command = "https://api.nfz.gov.pl/app-itl-api/queues?page=1&limit=25&format=json&case=1" + self.province + self.forChildren + self.provider + self.place + self.street + self.html_parse(self.locality)+ "&api-version=1.3"
+        self.api_command = "https://api.nfz.gov.pl/app-itl-api/queues?page=1&limit=25&format=json&case=1" + self.province + self.forChildren + self.provider + self.place + self.street + self.locality + "&api-version=1.3"
         print(self.api_command)
 
 
     def getting_json(self):
+
 
         try:
 
@@ -87,12 +90,11 @@ class ApiTranslation():
         
         except Exception as e:
             print(f"An error occurred: {e}")
-
+            
 
     def response_generator(self, json_data: list):
 
         data_list = []
-
 
         for record in json_data:
 
@@ -108,38 +110,57 @@ class ApiTranslation():
                                 'visit_name' : None,
                                 'phone': None
                              }
-            
-            temp_location_template['city'] = record['attributes']['locality']
-            temp_location_template['street'] = record['attributes']['address']
+            if self.disabled:    
 
-            temp_data_template['queue_id'] = record['id']
-            temp_data_template['place_name'] = record['attributes']['provider']
-            temp_data_template['location'] = temp_location_template
-            temp_data_template['visit_date'] = record['attributes']['dates']['date']
-            temp_data_template['visit_name'] = record['attributes']['benefit']
-            temp_data_template['phone'] = record['attributes']['phone']
-            
+                if record['attributes']['ramp'] == "Y":
 
-            data_list.append(temp_data_template)
+                    temp_location_template['city'] = record['attributes']['locality']
+                    temp_location_template['street'] = record['attributes']['address']
 
+                    temp_data_template['queue_id'] = record['id']
+                    temp_data_template['place_name'] = record['attributes']['provider']
+                    temp_data_template['location'] = temp_location_template
+                    temp_data_template['visit_date'] = record['attributes']['dates']['date']
+                    temp_data_template['visit_name'] = record['attributes']['benefit']
+                    temp_data_template['phone'] = record['attributes']['phone']
+                    
+
+                    data_list.append(temp_data_template)
+                
+                else: pass
+
+            else:
+
+                temp_location_template['city'] = record['attributes']['locality']
+                temp_location_template['street'] = record['attributes']['address']
+
+                temp_data_template['queue_id'] = record['id']
+                temp_data_template['place_name'] = record['attributes']['provider']
+                temp_data_template['location'] = temp_location_template
+                temp_data_template['visit_date'] = record['attributes']['dates']['date']
+                temp_data_template['visit_name'] = record['attributes']['benefit']
+                temp_data_template['phone'] = record['attributes']['phone']
+                
+                data_list.append(temp_data_template)
 
         return data_list
 
-d = {"specialist": "",
-    "province": "05",
-    "forChildren": False,
-    "provider": "",
-    "place": "stomatolog",
-    "street": "",
-    "locality": "",
-    "disabled": "",
-    "elevator": ""}
+
+
+# d = {"specialist": "",
+#     "province": "01",
+#     "forChildren": False,
+#     "provider": "",
+#     "place": "",
+#     "street": "",
+#     "locality": "Oława",
+#     "disabled": "",
+#     "elevator": ""}
 
 # c = ApiTranslation()
 # c.get_request(d)
 # c.constructing_filters()
 # c.generating_api_command()
-# c.getting_json()
-
+# print(c.getting_json())
 
 
